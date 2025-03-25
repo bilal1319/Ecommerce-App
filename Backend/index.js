9bf3ec1c1e56c37cc3203069443746ea829ccc75
bilal1319 <bilalkhsherwani1319@gmail.com>
2025-03-25 10:57:31 +0500

    Updated path

--- Backend/index.js
+++ Backend/index.js
@@ -49,7 +49,7 @@ if(process.env.NODE_ENV === 'production') {
     })
   }
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT,  () => {
     console.log("Server is running on port " + PORT);
     
 } )
